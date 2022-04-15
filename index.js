76b346b26a64c69665e988983ce2483e5e166f89
ColinBuyck <colin.buyck@gmail.com>
2022-04-15 02:24:38 -0400

    pleasee

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-
 app.get("/", async (req, res) => {
   const auth = new google.auth.GoogleAuth({
     keyFile: "credentials.json",
@@ -31,12 +30,12 @@ app.get("/", async (req, res) => {
     spreadsheetId,
     range: "Leaderboard",
   });
-  const teamArr = getRows.data.values.map((row, idx) => row[0]).slice(1)
+  const teamArr = getRows.data.values.map((row, idx) => row[0]).slice(1);
   const dashTeamArr = teamArr.map((team) => {
-    return team.replace(" ", "-")
-  })
-  const scoreArr = getRows.data.values.map((row, idx) => row[1]).slice(1)
-  res.render("index", {teamArr: dashTeamArr, scoreArr: scoreArr})
+    return team.replace(" ", "-");
+  });
+  const scoreArr = getRows.data.values.map((row, idx) => row[1]).slice(1);
+  res.render("index", { teamArr: dashTeamArr, scoreArr: scoreArr });
 });
 
-app.listen(1337, () => console.log("running on 1337"));
+app.listen(443, () => console.log("running on 1337"));
