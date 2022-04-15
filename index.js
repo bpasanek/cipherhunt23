0e896bcb23a67bb2d37a96976e3db4f40e080541
ColinBuyck <colin.buyck@gmail.com>
2022-04-15 15:14:52 -0400

    ugh

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { google } = require("googleapis");
 const app = express();
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"), express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
   const auth = new google.auth.GoogleAuth({
